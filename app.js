e8d237afe4d2831d0e05b4cb8001cf4a33d7cfac
Antoine-briz <antoine.brizard@outlook.fr>
2026-01-18 19:44:29 +0100

    Update app.js

--- app.js
+++ app.js
@@ -6617,15 +6617,15 @@ function etoFormHtmlCompact(prefix) {
                       </label>
                      
                       <label>SOR (cm²)
-        <input type="number" id="${prefix}-eto-ia-sor" step="0.01" min="0"/>
+        <input type="number" id="${prefix}-eto-im-sor" step="0.01" min="0"/>
       </label>
 
       <label>VR (mL)
-        <input type="number" id="${prefix}-eto-ia-vr" step="1" min="0"/>
+        <input type="number" id="${prefix}-eto-im-vr" step="1" min="0"/>
       </label>
 
       <label>FR (%)
-        <input type="number" id="${prefix}-eto-ia-fr" step="1" min="0" max="100"/>
+        <input type="number" id="${prefix}-eto-im-fr" step="1" min="0" max="100"/>
       </label>
 
                     </div>
@@ -7100,17 +7100,6 @@ function etoFormHtmlCompactPlastieAortique(prefix) {
                         <input type="number" id="${prefix}-eto-rm-p12" step="1" min="0"/>
                       </label>
                       
-                      <label>SOR (cm²)
-        <input type="number" id="${prefix}-eto-ia-sor" step="0.01" min="0"/>
-      </label>
-
-      <label>VR (mL)
-        <input type="number" id="${prefix}-eto-ia-vr" step="1" min="0"/>
-      </label>
-
-      <label>FR (%)
-        <input type="number" id="${prefix}-eto-ia-fr" step="1" min="0" max="100"/>
-      </label>
                     </div>
                   </div>
 
@@ -7139,6 +7128,18 @@ function etoFormHtmlCompactPlastieAortique(prefix) {
                       <label>P1/2T (ms)
                         <input type="number" id="${prefix}-eto-im-p12" step="1" min="0"/>
                       </label>
+                      <label>SOR (cm²)
+        <input type="number" id="${prefix}-eto-im-sor" step="0.01" min="0"/>
+      </label>
+
+      <label>VR (mL)
+        <input type="number" id="${prefix}-eto-im-vr" step="1" min="0"/>
+      </label>
+
+      <label>FR (%)
+        <input type="number" id="${prefix}-eto-im-fr" step="1" min="0" max="100"/>
+      </label>
+                      
                     </div>
                   </div>
                 </td>
