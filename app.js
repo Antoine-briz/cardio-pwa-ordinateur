190f0cf2f2a9f1a13a0d0704588a048c9b391f4a
Antoine-briz <antoine.brizard@outlook.fr>
2026-03-05 07:45:04 +0100

    Update app.js

--- app.js
+++ app.js
@@ -19729,7 +19729,7 @@ const CEC_PPT_TABLES = {
     ],
     [
       "Cardioplégie\nCf cardioplégies",
-      "Voie d’administration: XXX\nSolution de cardioplégie: XXX",
+      "XXX\nXXX",
       ""
     ],
 
