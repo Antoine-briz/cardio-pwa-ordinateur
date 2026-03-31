6d1f64ee08f8b84413d5b5f122994ac13c95bc0c
Antoine-briz <antoine.brizard@outlook.fr>
2026-03-31 20:44:56 +0200

    Update app.js

--- app.js
+++ app.js
@@ -22145,7 +22145,7 @@ const uploadToStorage = async (file) => {
   }
 
   const safeName = name.replace(/[^\w.\-]+/g, "_");
-  const path = `teaching/${Date.now()}__${safeName}`;
+  const path = `SARIC/teaching/${Date.now()}__${safeName}`;
   const ref = window.storage.ref().child(path);
 
   try {
