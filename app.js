edd81ecf6f423a5532a1266ccc05547d3d4eae12
Antoine-briz <antoine.brizard@outlook.fr>
2026-04-01 11:53:40 +0200

    Update app.js

--- app.js
+++ app.js
@@ -13165,7 +13165,7 @@ function setupReanPrescLogic() {
   // - Aspirine + Kardégic systématiques
   // - Anticoagulation préventive
   // - Retrait électrodes J1
-  const groupePréventif = new Set(["pc", "tsc", "rva-bio"]);
+  const groupePréventif = new Set(["pc", "tsc", "rva-bio", "plastie-ao"]);
 
   function update() {
     // 1) valeur brute du select
