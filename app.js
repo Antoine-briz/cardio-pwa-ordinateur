d2514977b2c1949c91462de54922ea28e34b8ac5
Antoine-briz <antoine.brizard@outlook.fr>
2026-04-22 14:33:37 +0200

    Update app.js

--- app.js
+++ app.js
@@ -943,7 +943,7 @@ function renderAnesthMenu() {
 
         <div class="card anesth-card" onclick="location.hash = '#/anesthesie/cardio-struct'">
           <img src="img/cardiostruct.png" class="menu-section-img">
-          <h3>Cardiologie structurelle</h3>
+          <h3>Cardiologie structurelle & rythmologie</h3>
         </div>
 
         <div class="card anesth-card" onclick="location.hash = '#/anesthesie/vasculaire'">
