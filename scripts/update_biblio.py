eba6377e7708927986bb0dd4e05316230c599bc2
Antoine-briz <antoine.brizard@outlook.fr>
2026-05-02 19:55:17 +0200

    Update selection logic to extend final_selection
    Increase the number of items selected per domain from 1 to 5.

--- scripts/update_biblio.py
+++ scripts/update_biblio.py
@@ -679,7 +679,7 @@ def update_publications() -> None:
         group = [item for item in items if item.domaine == domain]
         group = sorted(group, key=lambda x: x.score, reverse=True)
 
-        final_selection.extend(group[:1])
+        final_selection.extend(group[:5])
 
         final_selection = sorted(
         final_selection,
