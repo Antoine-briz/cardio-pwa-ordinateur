94229ec55df0b13c0f6ad0c756c5cf93d13397e0
Antoine-briz <antoine.brizard@outlook.fr>
2026-05-03 19:44:51 +0200

    Add EACTS journal logo to logoMap

--- app.js
+++ app.js
@@ -26842,7 +26842,8 @@ function biblioSourceHtml(source, domaine = "") {
     ["sfar", "img/journals/sfar.png"],
     ["srlf", "img/journals/srlf.png"],
     ["spilf", "img/journals/spilf.png"],
-    ["esc", "img/journals/esc.png"]
+    ["esc", "img/journals/esc.png"],
+    ["eacts", "img/journals/eacts.png"],
   ];
 
   const found = logoMap.find(([needle]) => key.includes(needle));
